884db37aae29404bde0eb40079243235439a9088
PierreAlainC <pierrea.cypres@gmail.com>
2024-06-04 17:14:51 +0200

    pages overview et connaîtreSandra + licorne

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -10,11 +10,33 @@ class MainController extends AbstractController
 {
     /**
      * 
+     * @Route("/licorne", name="unicorn")
+     */
+    public function unicorn(): Response
+    {
+        return $this->render('main/licorn.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    /**
+     * 
      * @Route("/", name="home")
      */
     public function index(): Response
     {
         return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    /**
+     * 
+     * @Route("/ConnaîtreSandra", name="overview")
+     */
+    public function overView(): Response
+    {
+        return $this->render('main/overview.html.twig', [
             'controller_name' => 'MainController',
         ]);
     }
