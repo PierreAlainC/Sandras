eaa7c715d318c2ef5b22c8ca8f06eda4a5f3ba71
PierreAlainC <pierrea.cypres@gmail.com>
2024-09-16 15:35:36 +0200

    page login + controlleurs + security.yaml + templates + page 403 + authenticator + vérification route + page default + ajout script JS

--- src/Controller/BackOffice/MainController.php
+++ src/Controller/BackOffice/MainController.php
@@ -1,24 +1,18 @@
 <?php
 
-namespace App\Controller\Backoffice;
+namespace App\Controller\BackOffice;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
-/**
- * @Route("/backoffice/movie")
- */
 class MainController extends AbstractController
 {
     /**
-     * @Route("/backoffice/main", name="app_backoffice_main")
+     * @Route("/backoffice", name="backoffice_main")
      */
     public function index(): Response
     {
-        return $this->render('backoffice/main/index.html.twig', [
-            'controller_name' => 'MainController',
-        ]);
+        return $this->render('backoffice/main/index.html.twig');
     }
 }
